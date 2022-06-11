e6fe07b525034807240644e62834f4f77c8c06c5
Seokmin Hong <ghdtjrald240@gmail.com>
2022-06-11 19:59:06 +0900

    Add comments

--- rustler_nif/native/calc/src/lib.rs
+++ rustler_nif/native/calc/src/lib.rs
@@ -25,6 +25,8 @@ fn send_message(env: Env, pid: LocalPid, message: String) {
     env.send(&pid, msg);
 }
 
+/// An asynchronous job that takes the given number of seconds to complete.
+/// The function returns `1`.
 async fn some_task(delay: f64) -> i32 {
     std::thread::sleep(std::time::Duration::from_secs_f64(delay));
     1
@@ -32,10 +34,15 @@ async fn some_task(delay: f64) -> i32 {
 
 #[rustler::nif]
 fn start_some_task(pid: LocalPid, delay: f64) {
+    // Spawn a new thread to run the task.
+    // The asynchronous job will run in the background.
     std::thread::spawn(move || {
+        // tokio runtime is used to run the asynchronous task.
         let runtime = tokio::runtime::Runtime::new().unwrap();
         runtime.block_on(async move {
             let ret = some_task(delay).await;
+            // Owned environment is the local environment of the NIF.
+            // It is used to encode the Rust values to NIF terms.
             let mut owned_env = OwnedEnv::new();
             owned_env.send_and_clear(&pid, |env| ("some_task", ret).encode(env));
         });
